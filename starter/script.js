e05a5e70cf0f9d2ab8b4ce3ab404cecc1a210e50
irorere juliet <irorerejuliet@gmail.com>
2025-08-09 06:34:20 +0100

    function declaration and expression

--- starter/script.js
+++ starter/script.js
@@ -1,4 +1,4 @@
-'use strict';
+
 
 let hasDriversLicense = false;
 const passTest = true;
@@ -9,3 +9,51 @@ if (hasDriversLicense) console.log('I can drive');
 
 
 const interface = 'Audio';
+
+function logger(){
+    console.log('My name is juliet!');
+}
+
+// This is calling, running invoking function
+logger()
+logger()
+logger()
+
+function fruitProcessor(apples, oranges){
+    const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+    return juice;
+}
+
+const appleJuice = fruitProcessor(5,0);
+console.log(appleJuice);
+console.log(fruitProcessor(5, 0));
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
+
+function juliet(june, may){
+    const birthmonth = `juliet was birth in ${june} and amina was birthed in ${may} samme year!`;
+    return birthmonth;
+}
+
+const ageJuliet = juliet(6, 8);
+console.log(ageJuliet);
+
+
+const gernini = juliet(2,8);
+console.log(gernini);
+
+// function declearation
+function calAge1(birthYear){
+    return 2037 - birthYear;
+}
+
+const age1 = calAge1(1991);
+console.log(age1)
+
+// function expression
+const calAge2 = function (birthYear){
+    return 2037 - birthYear;
+}
+const age2 = calAge2(1991);
+console.log(age1, age2);
