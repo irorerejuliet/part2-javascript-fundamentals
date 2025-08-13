486e853e24988722a69923ba4e116b08038c0b17
irorere juliet <irorerejuliet@gmail.com>
2025-08-13 10:42:01 +0100

    concluded function

--- starter/script.js
+++ starter/script.js
@@ -127,78 +127,104 @@
 // console.log(age1, age2);
 
 // Arrow functuon
-const calAge3 = birthYear => 2037 - birthYear;
-const age3 = calAge3(1991);
-console.log(age3);
+// const calAge3 = birthYear => 2037 - birthYear;
+// const age3 = calAge3(1991);
+// console.log(age3);
 
-const yearUntilRetirement = birthYear =>{
-    const age = 2037 - birthYear;
-    const retirement = 65 -age;
-    return retirement;
-}
-console.log(yearUntilRetirement(1991));
-
-
-const square = number => number * number;
-const totalNumber = square(10);
-console.log(totalNumber);
+// const yearUntilRetirement = birthYear =>{
+//     const age = 2037 - birthYear;
+//     const retirement = 65 -age;
+//     return retirement;
+// }
+// console.log(yearUntilRetirement(1991));
 
 
-
-const greet = name => `Hello ${name}`;
-console.log(greet("Juliet"));
-
-
-const isEven = n => n / 2 === 0;
-console.log(isEven(8));
-console.log(isEven(15));
-
-const max = (a, b) => (a >  b ? a : b);
-
-console.log(max(6, 7));
-console.log(max(17, 20));
-
-function cutFruitPieces (fruit){
-    return fruit * 4;
-}
-
-function fruitProcessor (apples, oranges){
-    const applePieces = cutFruitPieces(apples);
-    const orangesPieces = cutFruitPieces(oranges);
-
-    const juice = `Juice with ${applePieces}  pieces of apple and ${orangesPieces} pieces of orange.`;
-    return juice;
-}
-
-console.log(fruitProcessor(2, 3));
+// const square = number => number * number;
+// const totalNumber = square(10);
+// console.log(totalNumber);
 
 
-function generatePassword(length) {
-    function getRandomLetter() {
-        const letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        return letters[Math.floor(Math.random() * letters.length)];
-    }
 
-    function getRandomNumber() {
-        const numbers = "0123456789";
-        return numbers[Math.floor(Math.random() * numbers.length)];
-    }
+// const greet = name => `Hello ${name}`;
+// console.log(greet("Juliet"));
 
-    function getRandomSymbol() {
-        const symbols = "!@#$%^&*";
-        return symbols[Math.floor(Math.random() * symbols.length)];
-    }
 
-    let password = "";
-    const generators = [getRandomLetter, getRandomNumber, getRandomSymbol];
+// const isEven = n => n / 2 === 0;
+// console.log(isEven(8));
+// console.log(isEven(15));
 
-    for (let i = 0; i < length; i++) {
-        // Pick a random generator function
-        const randomGen = generators[Math.floor(Math.random() * generators.length)];
-        password += randomGen();
-    }
+// const max = (a, b) => (a >  b ? a : b);
 
-    return password;
-}
+// console.log(max(6, 7));
+// console.log(max(17, 20));
 
-console.log(generatePassword(8)); // Example: a7T@c9L!
+// function cutFruitPieces (fruit){
+//     return fruit * 4;
+// }
+
+// function fruitProcessor (apples, oranges){
+//     const applePieces = cutFruitPieces(apples);
+//     const orangesPieces = cutFruitPieces(oranges);
+
+//     const juice = `Juice with ${applePieces}  pieces of apple and ${orangesPieces} pieces of orange.`;
+//     return juice;
+// }
+
+// console.log(fruitProcessor(2, 3));
+
+
+// function generatePassword(length) {
+//     function getRandomLetter() {
+//         const letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+//         return letters[Math.floor(Math.random() * letters.length)];
+//     }
+
+//     function getRandomNumber() {
+//         const numbers = "0123456789";
+//         return numbers[Math.floor(Math.random() * numbers.length)];
+//     }
+
+//     function getRandomSymbol() {
+//         const symbols = "!@#$%^&*";
+//         return symbols[Math.floor(Math.random() * symbols.length)];
+//     }
+
+//     let password = "";
+//     const generators = [getRandomLetter, getRandomNumber, getRandomSymbol];
+
+//     for (let i = 0; i < length; i++) {
+//         // Pick a random generator function
+//         const randomGen = generators[Math.floor(Math.random() * generators.length)];
+//         password += randomGen();
+//     }
+
+//     return password;
+// }
+
+// console.log(generatePassword(8)); // Example: a7T@c9L!
+
+
+
+const calcAverage = (a, b, c) => (a + b + c) / 3;
+console.log(calcAverage(3, 4, 5));
+
+let scoreDolphins = calcAverage(44, 23, 71);
+let scoreKoalas = calcAverage(65, 54, 49);
+console.log(scoreDolphins, scoreKoalas);
+
+const checkWinner = function (avgDolphins, avgKoalas) {
+  if (avgDolphins >= 2 * avgKoalas) {
+    console.log(`Dolphins win (${avgDolphins} vs. ${avgKoalas})`);
+  } else if (avgKoalas >= 2 * avgDolphins) {
+    console.log(`Koalas win (${avgKoalas} vs. ${avgDolphins})`);
+  } else {
+    console.log("No team wins...");
+  }
+};
+
+checkWinner(scoreDolphins, scoreKoalas);
+
+const calAge1 = firstName => "Juliet IRORERE";
+
+console.log(calAge1());
+
