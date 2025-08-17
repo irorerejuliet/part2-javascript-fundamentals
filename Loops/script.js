69e3d17ca31d2d3c8370a841260daf4624dfc0c9
irorere juliet <irorerejuliet@gmail.com>
2025-08-17 16:56:49 +0100

    conluded loop

---
+++ Loops/script.js
@@ -0,0 +1,32 @@
+// console.log("Lifting weights repetition 1");
+// console.log("Lifting weights repetition 2");
+// console.log("Lifting weights repetition 3");
+// console.log("Lifting weights repetition 4");
+// console.log("Lifting weights repetition 5");
+// console.log("Lifting weights repetition 6");
+// console.log("Lifting weights repetition 7");
+// console.log("Lifting weights repetition 8");
+// console.log("Lifting weights repetition 9");
+// console.log("Lifting weights repetition 10");
+
+
+
+// For loop keeps running while codition is TRUE
+for (let rep = 1; rep <= 10; rep++){
+    console.log(`Lifting weights repetition ${rep}`);
+}
+
+const juliet = [
+    'Juliet',
+    'Irorere',
+    2037 - 19991,
+    'Teacher',
+    ['Micheal', 'Peter', 'Steven'],
+    true,
+]
+
+
+for (let i = 0; 1 < juliet.length; i++){
+    console.log(juliet[1]);
+    
+};
